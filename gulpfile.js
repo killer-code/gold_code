c4b5970a6ccc35621a29d27b3691e59c1df45d1c
Dmitry <killer_code@bk.ru>
2019-07-15 07:52:18 +0200

    Создание проекта, настройка gulp, добавление переменных цветов, миксины

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp         = require('gulp');
+
+const stylus       = require('gulp-stylus');
+const pug          = require('gulp-pug');
+
+const minifyCss    = require('gulp-minify-css');
+const minifyJs     = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require('gulp-rename');
+
+const browserSync  = require('browser-sync');
+const reload       = browserSync.reload;
+
+const paths = {
+  styl : ['src/styles/style.styl'],
+  pug  : ['src/templates/index.pug'],
+  js   : ['src/scripts/main.js'],
+};
+
+gulp.task('autoReload', function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  })
+});
+
+gulp.task('compilStyl', function() {
+  return gulp.src(paths.styl)
+    .pipe(stylus())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('src/dist'))
+    .pipe(reload({stream: true}))
+});
+
+gulp.task('compilPug', function() {
+  return gulp.src(paths.pug)
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+    .pipe(reload({stream: true}))
+});
+
+gulp.task('miniJs', function() {
+  return gulp.src(paths.js)
+    .pipe(minifyJs())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('src/dist'))
+    .pipe(reload({stream: true}))
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.styl, gulp.series('compilStyl'));
+    gulp.watch(paths.pug, gulp.series('compilPug'));
+    gulp.watch(paths.js, gulp.series('miniJs'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'autoReload'))
